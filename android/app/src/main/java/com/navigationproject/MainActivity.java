9f7217ea91e3f0a27ca5aa137f9142ba6d8f33f0
Thacher Hussain <thacher.hussain@gmail.com>
2022-06-08 19:03:46 -0400

    Updated App Name

--- android/app/src/main/java/com/navigationproject/MainActivity.java
+++ android/app/src/main/java/com/navigationproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NavigationProject";
+    return "Slytherin";
   }
 
   @Override
--- ios/SlytherinTests/SlytherinTests.m
+++ ios/SlytherinTests/SlytherinTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NavigationProjectTests : XCTestCase
+@interface SlytherinTests : XCTestCase
 
 @end
 
-@implementation NavigationProjectTests
+@implementation SlytherinTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
