a23022f9fb9ea1494c7868073638c29cf75f393a
Thacher Hussain <thacher.hussain@gmail.com>
2022-06-09 22:17:11 -0400

    config for absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@models': './src/models',
+          '@navigation-types': './src/navigation/types',
+          '@screens': './src/screens',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Box, Text, Image} from 'native-base'
 
-import {HomeProps} from '../navigation/types'
-import {Page, ButtonBase} from '../components'
+import {HomeProps} from '@navigation-types'
+import {Page, ButtonBase} from '@components'
 
 export const Home = ({navigation}: HomeProps) => {
   return (
